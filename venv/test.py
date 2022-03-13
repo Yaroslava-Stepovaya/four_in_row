fbb8e3ac0cba6ffd673328d34642fd91dc463275
Yaroslava-Stepovaya <yaraslava.st@gmail.com>
2022-03-13 13:07:00 +0300

    Разработаны правила выигрыша игроков, в игру можно играть человек против человека

---
+++ venv/test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pygame as pg
+
+# board = np.zeros((7, 6))
+# board[0][0] = 1
+# board[1][1] = 2
+# board[6][5] = 3
+# print(board)
+
+fonts = pg.font.get_fonts()
+print(len(fonts))
+for f in fonts:
+    print(f)
+
